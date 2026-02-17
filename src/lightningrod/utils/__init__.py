44342c8623bee58c68394ca329223e315a2b23c5
Bartolomej Kozorog <bartolomej.kozorog@gmail.com>
2026-02-17 20:24:13 +0100

    fix(utils): update dataset import

--- src/lightningrod/utils/__init__.py
+++ src/lightningrod/utils/__init__.py
@@ -1,12 +1,12 @@
-from .dataset_utils import (
+from .dataset import (
     add_rl_training_fields,
     filter_samples,
     test_train_split,
     flatten_samples,
     deduplicate_samples,
 )
-from lightningrod.utils.config import get_config_value
-from lightningrod.utils.rendering import render_sample
+from .config import get_config_value
+from .rendering import render_sample
 
 __all__ = [
     "add_rl_training_fields",
