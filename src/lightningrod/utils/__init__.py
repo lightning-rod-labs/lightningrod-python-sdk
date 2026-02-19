fc57216a3a1aa191d1abf4ecfeee2a4a6b4aece2
Bartolomej Kozorog <bartolomej.kozorog@gmail.com>
2026-02-19 17:44:32 +0100

    refactor to a cleaner api under training package

--- src/lightningrod/utils/__init__.py
+++ src/lightningrod/utils/__init__.py
@@ -1,10 +1,3 @@
-from .dataset import (
-    add_rl_training_fields,
-    filter_samples,
-    test_train_split,
-    flatten_samples,
-    deduplicate_samples,
-)
 from .config import get_config_value
 from .sample import create_sample
 from .metrics import (
@@ -14,10 +7,9 @@ from .metrics import (
     compute_multi_choice_consensus,
 )
 from .models import open_router_model
-from .rendering import render_sample
+from .metrics import compute_consensus, compute_metrics_summary
 
 __all__ = [
-    "add_rl_training_fields",
     "compute_consensus",
     "compute_consensus_summary",
     "compute_metrics_summary",
@@ -30,4 +22,5 @@ __all__ = [
     "open_router_model",
     "render_sample",
     "test_train_split",
+    "get_config_value",
 ]
