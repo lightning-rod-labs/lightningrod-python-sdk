794e6dc7ce6729d8159c6e8d0a0d6e226ccfd3f8
Bartolomej Kozorog <bartolomej.kozorog@gmail.com>
2026-05-01 15:32:07 +0200

    chore: bump version

--- src/lightningrod/__init__.py
+++ src/lightningrod/__init__.py
@@ -59,7 +59,7 @@ from lightningrod._generated.models import (
     WebSearchContextGenerator,
 )
 
-__version__ = "0.1.25"
+__version__ = "0.1.26"
 __all__ = [
     "preprocessing",
     "training",
